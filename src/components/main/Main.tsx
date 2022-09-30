1389b465f1bdeed1df13e4d140ec2210d5af4abb
wojtek <wojtekpietraszuk@gmail.com>
2022-09-30 11:06:22 +0200

    Add Boost component to render

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,13 +1,15 @@
 import Form from './Form';
 import AdvancedStatistics from './AdvancedStatistics';
+import Boost from './Boost';
 
 const Main: React.FC = () => {
   return (
     <main className='flex flex-col justify-center items-center mt-44 bg-primary-gray'>
-      <div className='mx-auto max-w-[1440px] w-full px-5 2xl:px-0'>
+      <div className='mx-auto max-w-[1440px] w-full px-5 2xl:px-0 pb-16'>
         <Form />
         <AdvancedStatistics />
       </div>
+      <Boost />
     </main>
   );
 };
